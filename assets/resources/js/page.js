64e778735a3fef2d8ce1f1aa8290e4e6d780d365
Coder <sevikash04@gmail.com>
2022-12-14 16:29:47 +0530

    upadte

--- assets/resources/js/page.js
+++ assets/resources/js/page.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
     Marq();
     Horizontal();
     Changehum();
-    splt();
+    // splt();
     hd();
     sku()
     Opaci();
@@ -1109,49 +1109,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-function splt() {
 
-    // ################ Split in span ##################
-    function splitLines(container, opentag, closingtag) {
-        // Get the spans in the paragraph 
-        var spans = container.children,
-
-            top = 0,
-
-            // set tmp as a string 
-            tmp = '';
-
-        // Put spans on each word, for now we use the <n> tag because, we want to save 5 bytes:)
-        container.innerHTML = container.textContent.replace(/\S+/g, '<n>$&</n>');
-
-        // Loop through each words (spans) 
-        for (let i = 0; i < spans.length; i++) {
-
-            // Get the height of each word
-            var rect = spans[i].getBoundingClientRect().top;
-
-            // If top is different as the last height of the word use a closingtag and use an opentag after that
-            if (top < rect) tmp += closingtag + opentag;
-            top = rect;
-
-            // Add the spans + space between each word
-            tmp += spans[i].textContent + ' ';
-        }
-
-        // Add the lines back to the paragraph 
-        container.innerHTML = tmp += closingtag;
-    }
-    // ################ END Split in span ################## 
-    var loop_split
-    var wid_w = window.innerWidth;
-    if (wid_w > 440) {
-        loop_split = document.querySelectorAll(".spline_p");
-
-        for (var i = 0; i < loop_split.length; i++) {
-            splitLines(loop_split[i], '<div><span>', '</span></div>');
-        }
-    }
-}
 
 
 function aliothShowcaseCarousel() {
