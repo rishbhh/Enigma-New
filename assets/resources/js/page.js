dc32bc4a8ae8223d5ebceccc4956ad7bb2d536ba
Coder <sevikash04@gmail.com>
2022-12-22 20:06:53 +0530

    update

--- assets/resources/js/page.js
+++ assets/resources/js/page.js
@@ -1383,3 +1383,22 @@ $(window).on('load', function() {
         });
     })
 }   
+
+function play(id) {
+    const audioElement = document.getElementById(id);
+  
+  resetAll();
+
+    if (audioElement.paused) {
+        audioElement.play();
+    }
+}
+
+function resetAll() {
+  const audioElements = document.querySelectorAll('audio[id^="sound-"]');
+  
+  audioElements.forEach(audioElement => {
+    audioElement.pause();
+    audioElement.currentTime = 0;
+  });
+}
